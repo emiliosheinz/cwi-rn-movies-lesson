4dc3b3f306a1b9e95c9a1d747e568100eedc0e9c
emilioheinz <emiliosheinz@gmail.com>
2020-06-23 15:43:18 -0300

    fix: removing view

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
-import {View} from 'react-native';
-
 import {DiscoverStack, ArtistsStack} from './stacks';
 
 const BottomTab = createBottomTabNavigator();
